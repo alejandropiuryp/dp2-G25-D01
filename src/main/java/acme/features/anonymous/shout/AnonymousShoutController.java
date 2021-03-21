c997033561bc61ad0e9482bf40b91f9b6e414d51
JJosePP <jjperezplata@gmail.com>
2021-03-21 20:21:27 +0100

    Task: Creating-the-entity-The controller finished
    Added the create shout controller

--- src/main/java/acme/features/anonymous/shout/AnonymousShoutController.java
+++ src/main/java/acme/features/anonymous/shout/AnonymousShoutController.java
@@ -1,6 +1,29 @@
 package acme.features.anonymous.shout;
 
+import javax.annotation.PostConstruct;
 
-public class AnonymousShoutController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import acme.entities.shouts.Shout;
+import acme.framework.components.BasicCommand;
+import acme.framework.controllers.AbstractController;
+import acme.framework.entities.Anonymous;
+
+@Controller
+@RequestMapping("/anonymous/shout")
+public class AnonymousShoutController extends AbstractController<Anonymous, Shout>{
+	
+	// Internal state --------------------------------------------------------
+	
+	@Autowired
+	private AnonymousShoutCreateService createService;
+	
+	// Constructors ----------------------------------------------------------
+	
+	@PostConstruct
+	private void initialise() {
+		super.addBasicCommand(BasicCommand.CREATE, this.createService);
+	}
 }
